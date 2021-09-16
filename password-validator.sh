02ab94be4fc718fa87a5b4bb4ec2ccf1efb84d53
NoamPeace <noam.benari@gmail.com>
2021-09-16 00:10:17 -0700

    colored output

--- password-validator.sh
+++ password-validator.sh
@@ -7,32 +7,34 @@
 # The script was written performance wise to use minimum variables as possible
 
 # Validates number of characters in the passed parameter ${#1}
-#   and outputs a message whether they are not ! equal or greater then 10
+#   and outputs a message whether they are not ! equal or greater then 10 (in red color)
 
 if ! [[ ${#1} -ge 10 ]]; then
-	echo "password is lower than 10 characters"
+	echo "$(tput setaf 1)password is lower than 10 characters"
 	exit 1
 fi
 
 # Validates alphabet and digits
 #   in the passed parameter $1
-#   and outputs a message whether they are not ! containing both alphabet and numbers
+#   and outputs a message whether they are not ! containing both alphabet and numbers (in red color)
 
 if ! [[ $1 =~ [[:alpha:]] && $1 =~ [[:digit:]] ]]; then
-	echo "password doesn't contain both alphabet and numbers"
+	echo "$(tput setaf 1)password doesn't contain both alphabet and numbers"
 	exit 1
 fi
 
 # Validates uppercase and lowercase letters
 #   in the passed parameter $1
-#   and outputs a message whether they are not ! containing both uppercase and & lowercase letters
+#   and outputs a message whether they are not ! containing both uppercase and & lowercase letters (in red color)
 
 if ! [[ $1 =~ [[:upper:]] && $1 =~ [[:lower:]] ]] ;then
-	echo "password doesn't contains both upppercase and lowercase"
+	echo "$(tput setaf 1)password doesn't contains both upppercase and lowercase"
 	exit 1
 fi
 
-# Password validation successfull, exit 0
+# Password validation successful, exit 0
+# Prints a successful message in green color
 
+echo "$(tput setaf 2)Successful"
 exit 0
 
